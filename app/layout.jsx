a82c336b0712d642125ee20193df5cec8a51d397
blackkiverson <blackkiverson@gmail.com>
2024-08-25 01:36:29 +0800

    desktop nav bar and animation with framer motion...

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,11 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+// components
+import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
+
 const inter = JetBrains_Mono({ 
   subsets: ["latin"], 
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -15,7 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header/>
+        <StairTransition/>
+        <PageTransition>
+          {children}
+        </PageTransition>
+      </body>
     </html>
   );
 }444
