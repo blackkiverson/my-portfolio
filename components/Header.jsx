d930c293c8ef027a9c2e9c2fa6c306030faf0a94
blackkiverson <blackkiverson@gmail.com>
2024-08-27 11:16:24 +0800

    handling mobile header and home screen for both mobile and web

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,32 +3,33 @@ import Link from "next/link";
 // components
 import { Button } from "./ui/button";
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto flex justify-between items-center">
-            <Link href="/">
-                <h1 className="text-4xl font-semibold">
-                    Samuel<span className="text-accent">.</span>
-                </h1>
-            </Link>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Blackk<span className="text-accent">.</span>
+          </h1>
+        </Link>
 
-            {/* desktop nav & hire me button */}
-            <div className="hidden xl:flex items-center gap-8">
-                <Nav />
-                <Link href="/contact">
-                    <Button>Hire me</Button>
-                </Link>
-            </div>
-
-            {/* mobile nav */}
-            <div className="xl:hidden">
-                <MobileNav />
-            </div>
+        {/* desktop nav & hire me button */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
         </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
